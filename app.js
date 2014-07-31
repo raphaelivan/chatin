cef6af4316369a6b9f210e4fde602a046d8e53f3
Raphael Ivan <raphaivan@gmail.com>
2014-07-31 16:51:16 -0300

    separating into layers

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
 var
   express      = require('express'),
   path         = require('path'),
-  favicon      = require('static-favicon'),
-  logger       = require('morgan'),
-  cookieParser = require('cookie-parser'),
-  bodyParser   = require('body-parser'),
-  routes       = require('./routes/index'),
-  users = require('./routes/users');
+  load         = require('express-load'),
+  app          = express();
 
-app = express();
-
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'assets')));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -55,6 +20,5 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,5 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (app) {
+  var home = app.controllers.home;
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+  app.get('/', home.index);
+};
