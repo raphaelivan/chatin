ee2c94dc8e9fc78eb9db382a37916800182d4289
Raphael Ivan <contato@raphaelivan.com.br>
2014-08-02 19:44:53 -0300

    contacts flow

--- controllers/contact.js
+++ controllers/contact.js
@@ -2,11 +2,65 @@ module.exports = function (app) {
   var ContactController = {
     index:  function (req, res) {
       var
-        user      = req.body.user,
-        params = { user: user };
+        user      = req.session.user,
+        contacts = user.contacts,
+        params = {
+          user: user,
+          contacts: contacts
+        };
 
         res.render('contacts/index', params);
+    },
+
+    create: function (req, res) {
+      var
+        user     = req.session.user,
+        contact = req.body.contact;
+      user.contacts.push(contact);
+      res.redirect('/contacts');
+    },
+
+    show: function (req, res) {
+      var
+        id         = req.params.id,
+        contact = req.session.user.contacts[id],
+        params = {
+          id: id,
+          contact: contact
+        }
+
+        res.render('contacts/show', params);
+    },
+
+    edit: function (req, res) {
+      var
+        id         = req.params.id,
+        user      = req.session.user,
+        contact =  user.contacts[id],
+        params = {
+          id: id,
+          user: user,
+          contact: contact
+        }
+
+      res.render('contacts/edit', params);
+    },
+
+    update: function (req, res) {
+      var
+        id         = req.params.id,
+        contact = req.body.contact;
+    },
+
+    destroy: function (req, res) {
+      var
+        id     = req.params.id,
+        user = req.session.user;
+
+      user.contacts.slice(id, 1);
+      res.redirect('/contacts');
     }
   }
+
   return ContactController;
 }
--- routes/contact.js
+++ routes/contact.js
@@ -2,4 +2,9 @@ module.exports = function (app) {
   var contact = app.controllers.contact;
 
   app.get('/contacts', contact.index);
+  app.get('/contact/:id', contact.show);
+  app.get('/contact/:id/edit', contact.edit );
+  app.post('/contact', contact.create);
+  app.put('/contact/:id', contact.update);
+  app.delete('/contact/:id', contact.destroy);
 };
