1caeb50914d7dafdb558a54735d5e9ec7b4e4209
Raphael Ivan <contato@raphaelivan.com.br>
2014-08-07 23:50:57 -0300

    Create chat contacts

---
+++ controllers/chat.js
@@ -0,0 +1,14 @@
+module.exports = function (app) {
+  var ChatController = {
+    index: function (req, res) {
+      var params  = {
+        user: req.session.user,
+        email: req.params.email
+      }
+
+      res.render('chat/index', params);
+    }
+  }
+
+  return ChatController;
+}
