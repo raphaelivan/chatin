5119d1755c31226e0da39105187f964a15aa8cf8
Raphael Ivan <raphaivan@gmail.com>
2014-08-13 02:40:34 -0300

    add some more icons

--- emoji.js
+++ emoji.js
@@ -5,7 +5,10 @@ EMOJI = {
   ':@': 'angry.png',
   ':\')': 'cry.png',
   ':D': 'grinning.png',
-  ':*': 'kissing_heart.png'
+  ':*': 'kissing_heart.png',
+  ';)': 'stuck_out_tongue_winking_eye.png',
+  '8|': 'sunglasses.png',
+  '><': 'stuck_out_tongue_closed_eyes.png'
 }
 
 exports.icons = EMOJI;
