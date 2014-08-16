20b7da98f95d5e7d046482a557d6019d9ddcede7
Raphael Ivan <raphaivan@gmail.com>
2014-08-16 06:32:14 -0300

    Controller Session Spec

---
+++ spec/session.js
@@ -0,0 +1,50 @@
+var
+  app     = require('../app'),
+  mocha   = require('mocha'),
+  request = require('supertest')(app);
+
+describe('Session Crontol', function () {
+  it('When the user is not logged in, redirect it to /', function (done) {
+    request
+    .get('/contacts')
+    .expect(302, done)
+    .expect('Moved Temporarily. Redirecting to /');
+  });
+
+  it('Create a session', function (done) {
+    var data = {
+        user: {
+        name: 'User',
+        email: 'user@chatin'
+      }
+    }
+
+    request
+    .post('/login')
+    .send(data)
+    .expect(302, done)
+    .expect('Moved Temporarily. Redirecting to /contacts');
+  });
+
+  it('Trying create a session without fill the form', function (done) {
+    var data = {
+        user: {
+        name: '',
+        email: ''
+      }
+    }
+
+    request
+    .post('/login')
+    .send(data)
+    .expect(302, done)
+    .expect('Moved Temporarily. Redirecting to /')
+  });
+
+  it('logout', function (done) {
+    request
+    .get('/logout')
+    .expect(302, done)
+    .expect('Moved Temporarily. Redirecting to /');
+  })
+});
