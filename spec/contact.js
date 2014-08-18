9f2468242b43e4a36765eb32aa03e3098b70f2d4
Raphael Ivan <raphaivan@gmail.com>
2014-08-18 15:04:39 -0300

    Test when user is logged

--- spec/contact.js
+++ spec/contact.js
@@ -4,12 +4,72 @@ var
   request = require('supertest')(app);
 
 describe('Controller Contacts', function (){
-  describe('When user is logged', function () {
+  describe('When user is  not logged', function () {
     it('should redirect to /', function (done) {
       request
         .get('/contacts')
         .expect(302, done)
         .expect('Moved Temporarily. Redirecting to /')
+    });
+  });
+
+
+  describe('When user is logged', function () {
+    beforeEach(function (done) {
+      var data = { user: {
+          name: 'User',
+          email: 'user@chatin'
+        }
+      };
+
+      request
+        .post('/login')
+        .send(data)
+
+      done();
+    });
+
+
+    it('should be able to see contacts list', function (done) {
+      request
+        .get('/contacts')
+        .expect('Logout')
+      done();
+    });
+
+
+    it('should be able to register a contact', function (done) {
+      var data = {
+          contact: {
+            name: 'UserII',
+            email: 'user2@chatin.com.br'
+          }
+      };
+
+      request
+        .post('/contact')
+        .send(data)
+        .expect(data.contact.name);
+      done();
+    });
+
+    it('should be able to see the contact info', function (done) {
+      var data = {
+          contact: {
+            name: 'UserII',
+            email: 'user2@chatin.com.br'
+          }
+      };
+
+      request
+        .post('/contact')
+        .send(data)
+
+      request
+        .get('/contact/'+data.contact.email)
+        .expect(data.contact.name)
+
+      done();
     });
   });
 });
